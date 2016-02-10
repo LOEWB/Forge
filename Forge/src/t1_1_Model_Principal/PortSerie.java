aacbbe9c93b0456a7d93ac1b110941f8ad36d27b
barneoudh <hector.barneoud@iut-valence.fr>
2016-02-10 18:58:07 +0100

    Completition of PortSerie class

--- Forge/src/t1_1_Model_Principal/PortSerie.java
+++ Forge/src/t1_1_Model_Principal/PortSerie.java
@@ -1,35 +1,75 @@
 package t1_1_Model_Principal;
 
-import java.util.Enumeration;
+import java.io.*;
+import javax.comm.*;
+import java.util.*;
+
+import gnu.io.CommPort;
 import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
 
-public class PortSerie {
-private Enumeration ports;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
-public PortSerie() {
-	this.ports = CommPortIdentifier.getPortIdentifiers();
+public class PortSerie
+{
+    static Enumeration ports;
+	ArrayList<String> listePorts; 
+    int debit;
+    
+    static OutputStream outStream;
+    SerialPort serialPort;
+    CommPort commPort;
+    CommPortIdentifier portIdentifier;
 
-	int i = 1;
-	while (ports.hasMoreElements())
-	{
-	CommPortIdentifier port = (CommPortIdentifier) ports.nextElement();
-	System.out.println("Port n°"+i++);
-	System.out.println("\tNom\t:\t"+port.getName());
-	String type = null;
-	if (port.getPortType() == CommPortIdentifier.PORT_SERIAL) type = "Serie";
-	else type = "Parallèle";
-	System.out.println("\tType\t:\t"+type);
-	String etat = null;
-	if (port.isCurrentlyOwned()) etat = "Possédé par "+port.getCurrentOwner();
-	else etat = "Libre";
-	System.out.println("\tEtat\t:\t"+etat+"\n");
+    
+    public PortSerie() throws Exception{
+    
+        ports = CommPortIdentifier.getPortIdentifiers();
+        
+        while(ports.hasMoreElements())
+        {
+        	portIdentifier = (CommPortIdentifier)ports.nextElement();
+            
+            if (portIdentifier.getPortType() == CommPortIdentifier.PORT_SERIAL) listePorts.add(portIdentifier.getName());
+        
+        }
 	}
-	
+
+
+public ArrayList<String> getListePorts() 
+{
+	return this.listePorts;
 }
 
-public void envoyer(String contenu) {
+boolean setPort(String port, int Debit) {
+
 	
+	if(Debit == 0) this.debit = 2400;
+	else this.debit = Debit;
+	
+    this.portIdentifier = CommPortIdentifier.getPortIdentifier(port);
+    
+    if (portIdentifier.isCurrentlyOwned() ) return false;
+    
+    this.commPort = portIdentifier.open("Forge Software on " + port,10000);
+    
+    if (!commPort instanceof SerialPort) return false;
+    
+    this.serialPort = (SerialPort) commPort;
+    
+    serialPort.setSerialPortParams(this.debit,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
+
+	return true;
 }
 
+void envoyer(String donnees) {
+	
+    OutputStream outstream = this.serialPort.getOutputStream();
+    outstream.write(donnees.getBytes());
 
+}
+ 
 }
